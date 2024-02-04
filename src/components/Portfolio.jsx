ed11c85cb501c6b1e6555580de42f3715f11a8e7
harshahossain <harshahossain@gmail.com>
2024-02-04 21:49:14 +0600

    updated

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import slickDress from "../assets/portfolio/slickDress.png";
+import newsPortal from "../assets/portfolio/newsPortal.png";
+import dadJokes from "../assets/portfolio/dadJokes.png";
+import getCard from "../assets/portfolio/getACard.png";
+
+export default function Portfolio() {
+  return <div>Portfolio</div>;
+}
