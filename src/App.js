a6ca6cd82d6a0bc70a2fbfb895bbb07423fa22c9
harshahossain <harshahossain@gmail.com>
2024-02-06 00:42:54 +0600

    created contact & updated App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
@@ -8,12 +9,12 @@ import SocialLinks from "./components/SocialLinks";
 function App() {
   return (
     <div>
-      {" "}
       <NavBar />
       <Home />
       <About />
       <Portfolio />
       <Experience />
+      <Contact />
       {/*  */}
       <SocialLinks />
     </div>
