649cac24b92aa41a4b68efb824a2ad710962d3f0
harshahossain <harshahossain@gmail.com>
2024-02-05 11:03:31 +0600

    updated portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,7 +3,27 @@ import slickDress from "../assets/portfolio/slickDress.png";
 import newsPortal from "../assets/portfolio/newsPortal.png";
 import dadJokes from "../assets/portfolio/dadJokes.png";
 import getCard from "../assets/portfolio/getACard.png";
+import ticTac from "../assets/portfolio/ticTac.png";
 
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <div>
+      <main>
+        <div>
+          <p>Portfolio</p>
+          <p>Check out some of work here.</p>
+        </div>
+        {/* ↓↓↓ cards  */}
+        <section>
+          <div>
+            <img src={slickDress} alt="" />
+            <div>
+              <button>Demo</button>
+              <button>Code</button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
