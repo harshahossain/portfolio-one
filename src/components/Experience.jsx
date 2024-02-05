3d9dfb563130e999b49139361742018345aea640
harshahossain <harshahossain@gmail.com>
2024-02-06 00:37:24 +0600

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -14,9 +14,9 @@ export default function Experience() {
     { id: 1, src: html, title: "HTML", style: "shadow-orange-500" },
     { id: 2, src: css, title: "CSS", style: "shadow-blue-500" },
     { id: 3, src: javaScript, title: "JavaScript", style: "shadow-yellow-500" },
-    { id: 4, src: reactJs, title: "React", style: "shadow-blue-600" },
+    { id: 4, src: reactJs, title: "React", style: "shadow-blue-700" },
     { id: 5, src: tailwind, title: "Tailwind", style: "shadow-sky-400" },
-    { id: 6, src: nodejs, title: "NodeJS", style: "shadow-green-200" },
+    { id: 6, src: nodejs, title: "NodeJS", style: "shadow-green-300" },
     { id: 7, src: gitHub, title: "GitHub", style: "shadow-gray-400" },
     { id: 8, src: mongo, title: "MongoDB", style: "shadow-green-500" },
   ];
@@ -35,12 +35,18 @@ export default function Experience() {
             These are the technologies I've worked with
           </p>
         </div>
-        {/*  */}
+        {/* card sectin */}
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-          <div className="shadow-md hover:scale-105 duration-500 py-2 rounded-lg">
-            <img src={html} alt="" className="w-20 mx-auto" />
-            <p className="mt-4">HTML</p>
-          </div>
+          {/* loopign here↓↓↓ after grid↑↑↑*/}
+          {techs.map(({ id, src, title, style }) => (
+            <div
+              key={id}
+              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+            >
+              <img src={src} alt={title} className="w-20 mx-auto" />
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
         </div>
       </section>
     </div>
